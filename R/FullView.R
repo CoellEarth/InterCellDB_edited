bc2134c1365bedaa75667e804e3a1c4a047155be
Coell Shen <161328923+CoellEarth@users.noreply.github.com>
2024-08-11 16:01:13 +0800

    delete row 369

--- R/FullView.R
+++ R/FullView.R
@@ -366,9 +366,6 @@ AnalyzeInterInFullView <- function(
 				if (!is.null(property$location) && length(property$location) > 0) {
 					data.location <- subset(data.location, GO.Term.target %in% property$location)
 				}
-				if (!is.null(property$location.score) && length(property$location.score) > 0) {
-					data.location <- subset(data.location, score %in% property$location.score)
-				}
 				
 				if (!is.null(property$type) && length(property$type) > 0) {
 					data.type <- subset(data.type, Keyword.Name %in% property$type)
